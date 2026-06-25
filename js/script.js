71d3714692907507ff32393ae240440245a06509
ampiremurhimanya <167401625+ampiremurhimanya@users.noreply.github.com>
2026-06-25 20:15:38 +0300

    implement phone search functionality

--- js/script.js
+++ js/script.js
@@ -4,11 +4,28 @@ function validateForm() {
     let email = document.getElementById("email").value;
 
     if (name === "" || email === "") {
-
         alert("Please fill in all required fields.");
-
         return false;
     }
 
     return true;
+}
+
+function searchPhone() {
+
+    let input =
+    document.getElementById("search").value.toLowerCase();
+
+    let phones =
+    document.querySelectorAll(".phone-card");
+
+    phones.forEach(function(phone){
+
+        if(phone.innerText.toLowerCase().includes(input)){
+            phone.style.display = "";
+        } else {
+            phone.style.display = "none";
+        }
+
+    });
 }
